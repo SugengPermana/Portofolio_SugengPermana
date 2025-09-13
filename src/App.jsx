8f443285e7d9ecd72c58b1bab8d9e5ba431afda3
Sugeng Permana Desembry <mylistensong6@gmail.com>
2025-09-13 11:47:26 +0700

    create Proyek,Contact,Form submission, footer & preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Image from "./data.js"
-import { listTools } from "./data.js"
+import { listTools,listProyek } from "./data.js"
 
 function App() {
 
@@ -7,10 +7,10 @@ function App() {
     <>
     {/* Hero Section */}
       <div className="hero grid md:grid-cols-2 pt-10 items-center xl:gap-0 gap-6 grid-cols-1">
-        <div>
+        <div className="animate__animated animate__fadeInUp animate__delay-3s">
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2sl">
             {/* image kecil */}
-              <img src={Image.HeroImage} alt="Hero Image" className="w-10 rounded-md" />
+              <img src={Image.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy"/>
               <q>Kode yang indah, lahir dari Ketekunan 👍</q>
           </div>
           <h1 className="text-5xl/tight font-bold mb-6">Hi, I`m Sugeng Permana Desembry</h1>
@@ -23,7 +23,7 @@ function App() {
           </div>
         </div>
             {/* Image besar */}
-              <img src={Image.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
+              <img src={Image.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto animate__animated animate__fadeInUp animate__delay-4s" loading="lazy" />
       </div>
       {/* Hero section */}
 
@@ -31,12 +31,12 @@ function App() {
         <div className="tentang mt-32 py-10">
           <div className="xl:w-2/3 lg:w3/4 w-full mx-auto
           p-7 bg-zinc-800 rounded-md">
-            <img src={Image.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
+            <img src={Image.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy" />
             <p className="text-base/loose mb-10">
               Hi, My name is Sugeng Permana Desembry, saya seorang junior fullstack engineer, keahlian saya menggunakan bahasa HTML,Css,JS dan untuk framework React, Tailwind, Next.js  
             </p>
             <div className="flex items-center justify-between">
-              <img src={Image.HeroImage} alt="image" className="w-12 rounded-md sm:block hidden" />
+              <img src={Image.HeroImage} alt="image" className="w-12 rounded-md sm:block hidden" loading="lazy" />
               <div className="flex items-center gap-6">
                 <div>
                   <h1 className="text-4xl mb-1">
@@ -59,7 +59,7 @@ function App() {
             <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">Berikut ini tools yang biasa saya pakai untuk ngoding</p>
             <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4">
 
-              {listTools.map((tool) => (
+              {listTools.map(tool => (
                 <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
                   <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" />
                 <div>
@@ -68,10 +68,65 @@ function App() {
                 </div>
               </div>
                 ))}
+
             </div>
           </div>
         </div>
       {/* Tentang */}
+
+      {/* Proyek */}
+      <div className="proyek mt-32 py-10">
+        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50">Berikut ini beebrapa proyek yang telah saya buat</p>
+
+        <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listProyek.map(proyek => (
+            <div className="p-4 bg-zinc-800 rounded-md" key={proyek.id}>
+              <img src={proyek.gambar} alt="Proyek image" loading="lazy"/>
+              <div>
+                <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+                <p className="text-base/loose mb-4">{proyek.desk}</p>
+
+                <div className="flex flex-wrap gap-2">
+                  {proyek.tools.map((tool, index) => (
+                    <p className="py-1 px-3 border border-zinc-800 bg-zinc-600 rounded-md font-semibold " key={index}>{tool}</p>
+                  ))}
+                </div>
+                <div className="mt-8 text-center">
+                  <a href="#" className="bg-violet-700 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-600">Lihat Website</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Proyek */}
+
+      {/* kontak */}
+      <div className="kontak mt-32 sm:p-10 p-0">
+        <h1 className="text-4xl mb-2 font-bold text-center">Kontak</h1>
+        <p className="text-base/loose text-center mb-10 opacity-50">Mari Berhubung dengan saya</p>
+        <form action="https://formsubmit.co/sugengpermanadesembry@gmail.com" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off" >
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Nama Lengkap</label>
+              <input type="text" name="nama" placeholder="Masukan Nama" className="border border-zinc-500 p-2 rounded-md" required />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Email</label>
+              <input type="email" name="email" placeholder="Masukan Email" className="border border-zinc-500 p-2 rounded-md" required />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="pesan" className="font-semibold">Pesan</label>
+              <textarea name="pesan" id="pesan" cols={45} rows={7} placeholder="Masukan Pesan" className="border border-zinc-500 p-2 rounded-md" required ></textarea>
+            </div>
+            <div className="text-center">
+              <button type="submit" className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-600 hover:bg-violet-600">Kirim Pesan</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {/* kontak */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,19 @@ import App from './App.jsx'
 import Navbar from './components/navbar.jsx'
 
 import 'remixicon/fonts/remixicon.css'
+import Footer from './components/footer.jsx'
+import Prelooder from './components/Prelooder.jsx'
+import 'animate.css';
 
 // Untuk Render Coding Di sini 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Prelooder />
     <div className="container mx-auto px-4">
     <Navbar />
     <App />
+    <Footer />
     </div>
   </StrictMode>,
 )
