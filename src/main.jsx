678a9cbf4551d0a0e21802848810ebffc790d51c
Sugeng Permana Desembry <mylistensong6@gmail.com>
2025-09-14 08:39:35 +0700

    revisi preloader

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Navbar from './components/navbar.jsx'
+import { StrictMode, useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
+import Preloader from "./components/Preloader.jsx";
 
-import 'remixicon/fonts/remixicon.css'
-import Footer from './components/footer.jsx'
-import Prelooder from './components/Prelooder.jsx'
-import 'animate.css';
+import "remixicon/fonts/remixicon.css";
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+// init animasi scroll
 AOS.init();
-// Untuk Render Coding Di sini 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Prelooder />
+function Main() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Preloader />; // tampil preloader dulu
+  }
+
+  return (
     <div className="container mx-auto px-4">
-    <Navbar />
-    <App />
-    <Footer />
+      <Navbar />
+      <App />
+      <Footer />
     </div>
-  </StrictMode>,
-)
+  );
+}
+
+// Render ke root
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Main />
+  </StrictMode>
+);
